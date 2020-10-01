c9e1596fbddf320c0a9569daeeaca3f6a70d3ca0
Sarah Wong <swong3310@gmail.com>
2020-10-01 00:44:20 +0000

    Creating file reader function

--- produce_summary.py
+++ produce_summary.py
@@ -2,14 +2,14 @@ print("Day 1")
 the_file = open("um-deliveries-20140519.txt")
 for line in the_file:
     line = line.rstrip()
-    words = line.split('|')
+    words = line.split("|")
+    # print(words)
 
     melon = words[0]
     count = words[0]
     amount = words[0]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+    print("Delivered {} {}s for total of ${}".format(count, melon, amount))
 the_file.close()
 
 
@@ -17,14 +17,13 @@ print("Day 2")
 the_file = open("um-deliveries-20140520.txt")
 for line in the_file:
     line = line.rstrip()
-    words = line.split('|')
+    words = line.split("|")
 
     melon = words[0]
     count = words[0]
     amount = words[0]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+    print("Delivered {} {}s for total of ${}".format(count, melon, amount))
 the_file.close()
 
 
@@ -32,12 +31,31 @@ print("Day 3")
 the_file = open("um-deliveries-20140521.txt")
 for line in the_file:
     line = line.rstrip()
-    words = line.split('|')
+    words = line.split("|")
 
     melon = words[0]
     count = words[0]
     amount = words[0]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+    print("Delivered {} {}s for total of ${}".format(count, melon, amount))
 the_file.close()
+
+# Create a function:
+def file_reader(file):
+
+    for line in file:
+        line = line.rstrip()
+        words = line.split("|")
+
+        melon = words[0]
+        count = words[0]
+        amount = words[0]
+
+        print("Delivered {} {}s for total of ${}".format(count, melon, amount))
+    file.close()
+
+    # Function call:
+
+
+# file_reader(file=open("um-deliveries-20140521.txt"))
+
